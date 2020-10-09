a0a4e326fb9005627e3c6b192591908f41fc9f34
ts9417 <96325570+ts9417@users.noreply.github.com>
2020-10-09 15:27:23 +0000

    bumped version

--- Pods/Alamofire/Source/MultipartFormData.swift
+++ Pods/Alamofire/Source/MultipartFormData.swift
@@ -393,4 +393,22 @@ open class MultipartFormData {
         self.bodyParts.last?.hasFinalBoundary = true
 
         for bodyPart in self.bodyParts {
-         
+            try write(bodyPart, to: outputStream)
+        }
+    }
+
+    // MARK: - Private - Body Part Encoding
+
+    private func encode(_ bodyPart: BodyPart) throws -> Data {
+        var encoded = Data()
+
+        let initialData = bodyPart.hasInitialBoundary ? initialBoundaryData() : encapsulatedBoundaryData()
+        encoded.append(initialData)
+
+        let headerData = encodeHeaders(for: bodyPart)
+        encoded.append(headerData)
+
+        let bodyStreamData = try encodeBodyStream(for: bodyPart)
+        encoded.append(bodyStreamData)
+
+ 
