c1d1d1299c53edda40efa6ff877f18f7955e3502
ts9417 <96325570+ts9417@users.noreply.github.com>
2022-03-13 21:15:36 +0000

    wwww

--- Pods/SwiftChart/Source/Chart.swift
+++ Pods/SwiftChart/Source/Chart.swift
@@ -9,4 +9,14 @@ import UIKit
 
 public protocol ChartDelegate: class {
 
-    /
+    /**
+    Tells the delegate that the specified chart has been touched.
+
+    - parameter chart: The chart that has been touched.
+    - parameter indexes: Each element of this array contains the index of the data that has been touched, one for each 
+      series. If the series hasn't been touched, its index will be nil.
+    - parameter x: The value on the x-axis that has been touched.
+    - parameter left: The distance from the left side of the chart.
+
+    */
+    func didTouchChart(_ chart: Chart, indexes: [In
