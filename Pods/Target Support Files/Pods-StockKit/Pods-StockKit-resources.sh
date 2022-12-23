6a125a3806eb7d2408422a08cd1f068d04df08e7
ts9417 <96325570+ts9417@users.noreply.github.com>
2022-12-23 14:59:50 +0000

    add txt

---
+++ Pods/Target Support Files/Pods-StockKit/Pods-StockKit-resources.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+
+RESOURCES_TO_COPY=${PODS_ROOT}/resources-to-copy-${TARGETNAME}.txt
+> "$RESOURCES_TO_COPY"
+
+XCASSET_FILES=()
+
+case "${TARGETED_DEVICE
