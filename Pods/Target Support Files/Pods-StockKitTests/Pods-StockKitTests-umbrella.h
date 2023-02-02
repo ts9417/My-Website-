e78cd6890f99b0ded1bdb3c66091182f6610e962
ts9417 <96325570+ts9417@users.noreply.github.com>
2023-02-02 18:50:05 +0000

    x86: Reset the debug_stack update counter

--- Pods/Target Support Files/Pods-StockKitTests/Pods-StockKitTests-umbrella.h
+++ Pods/Target Support Files/Pods-StockKitTests/Pods-StockKitTests-umbrella.h
@@ -1,2 +1,16 @@
 #ifdef __OBJC__
-#impo
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_StockKitTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_StockKitTestsVersionString[];
+
