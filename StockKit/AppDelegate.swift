f484aa2c08b0abbbb47618786fa5af5d05c0b98b
ts9417 <96325570+ts9417@users.noreply.github.com>
2023-02-08 18:37:43 +0000

    Some more changes

--- StockKit/AppDelegate.swift
+++ StockKit/AppDelegate.swift
@@ -1,2 +1,22 @@
 //
-//  App
+//  AppDelegate.swift
+//  StockKit
+//
+//  Created by Nathan Tannar on 7/20/17.
+//  Copyright © 2017 Nathan Tannar. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import NTComponents
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        UIApplication.shared
