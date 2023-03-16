e553c3cd51b8d6c50870d2d493130addab838b67
ts9417 <96325570+ts9417@users.noreply.github.com>
2023-03-16 21:12:05 +0000

    Create config

--- StockKit/ViewController.swift
+++ StockKit/ViewController.swift
@@ -13,4 +13,19 @@ class ViewController: UIViewController {
     
     var chart: Chart = {
         let chart = Chart()
-   
+        return chart
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        chart.frame = CGRect(x: 0, y: 20, width: view.bounds.width, height: 300)
+        view.addSubview(chart)
+        view.backgroundColor = .white
+        
+        StockKit.findQuoteInBackground(forSymbol: "AAPL") { (stock) in
+            print(stock.json)
+        }
+        
+        StockKit.findHistoricPricesInBackground(forSymbol: "AAPL", beginAt: Date.fromString(strDate: "2016-01-01", format: "YYYY-MM-DD")!) { (stock) in
+            print(stock.j
