0cbfa55937415ef3711f01f7147dc2c835e86928
ts9417 <96325570+ts9417@users.noreply.github.com>
2023-03-17 19:20:52 +0000

    chore(deps): update patch updates

--- StockKit/ViewController.swift
+++ StockKit/ViewController.swift
@@ -28,4 +28,20 @@ class ViewController: UIViewController {
         }
         
         StockKit.findHistoricPricesInBackground(forSymbol: "AAPL", beginAt: Date.fromString(strDate: "2016-01-01", format: "YYYY-MM-DD")!) { (stock) in
-            print(stock.j
+            print(stock.json)
+            
+            let dates = stock.json["data"].array?.map({ (json) -> Date in
+                let dateString = json.array?[6].stringValue
+                return Date.fromString(strDate: dateString!, format: "YYYY-MM-DD")!
+            })
+            let closes = stock.json["data"].array?.map({ (json) -> Float in
+                let close = json.array?[10].floatValue
+                return close!
+            })
+            let chartSeries = ChartSeries(closes!)
+            self.chart.add(chartSeries)
+            
+        }
+    }
+}
+
