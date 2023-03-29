938797abe79b565de678c177f26baef474e88a24
ts9417 <96325570+ts9417@users.noreply.github.com>
2023-03-29 14:59:40 +0000

    refactor packages

---
+++ StockKitTests/StockKitTests.swift
@@ -0,0 +1,20 @@
+//
+//  StockKitTests.swift
+//  StockKitTests
+//
+//  Created by Nathan Tannar on 7/20/17.
+//  Copyright © 2017 Nathan Tannar. All rights reserved.
+//
+
+import XCTest
+@testable import StockKit
+
+class StockKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown c
